ee63e28f35803cd16af1df4fbc14144a42eff2b4
Pavlo Romanenko <pavlo.romanenko@hazelcast.com>
2021-08-12 11:34:30 +0300

    Matches endpoint

--- src/main/java/com/hazelcast/hazelbet/controller/MatchController.java
+++ src/main/java/com/hazelcast/hazelbet/controller/MatchController.java
@@ -1,2 +1,24 @@
-package com.hazelcast.hazelbet.controller;public class MatchController {
+package com.hazelcast.hazelbet.controller;
+
+import com.hazelcast.hazelbet.controller.model.Match;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/matches")
+
+public class MatchController {
+
+    @GetMapping
+    public List<Match> listMatches() {
+        // TODO read from map
+        return List.of(
+                Match.builder().id(1l).firstTeam("Barcelona").secondTeam("Madrid").winFirst(2.1).draw(2.2).winSecond(1.7).build(),
+                Match.builder().id(2l).firstTeam("Dynamo").secondTeam("Zorya").winFirst(1.5).draw(2.7).winSecond(3.3).build()
+        );
+    }
+
 }
--- src/main/java/com/hazelcast/hazelbet/controller/model/Match.java
+++ src/main/java/com/hazelcast/hazelbet/controller/model/Match.java
@@ -1,2 +1,17 @@
-package com.hazelcast.hazelbet.controller.model;public class Match {
+package com.hazelcast.hazelbet.controller.model;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class Match {
+
+    long id;
+    String firstTeam;
+    String secondTeam;
+    double winFirst;
+    double draw;
+    double winSecond;
+
 }
