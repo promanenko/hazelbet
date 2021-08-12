5b43b375075d9550e68932c2298eaa6cf02f00a8
Pavlo Romanenko <pavlo.romanenko@hazelcast.com>
2021-08-12 10:49:49 +0300

    init

---
+++ src/main/java/com/hazelcast/hazelbet/HazelbetApplication.java
@@ -0,0 +1,13 @@
+package com.hazelcast.hazelbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelbetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazelbetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hazelcast/hazelbet/HazelbetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazelcast.hazelbet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazelbetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
